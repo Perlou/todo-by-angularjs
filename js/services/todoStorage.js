5dabba8dcda36089406a5a6533b61865befd2382
Perlou <a87387875@163.com>
2016-06-02 12:57:10 +0800

    build

--- js/services/todoStorage.js
+++ js/services/todoStorage.js
@@ -1,18 +1,12 @@
-/*global angular */
-
 /**
- * Services that persists and retrieves todos from localStorage or a backend API
- * if available.
- *
- * They both follow the same API, returning promises for all changes to the
- * model.
+ * @author Perlou
+ * @todoStorage.js
  */
+
 angular.module('todomvc')
 	.factory('todoStorage', function ($http, $injector) {
 		'use strict';
 
-		// Detect if an API backend is present. If so, return the API module, else
-		// hand off the localStorage adapter
 		return $http.get('/api')
 			.then(function () {
 				return $injector.get('api');
