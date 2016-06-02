bc7d65de84c00ec6eb8f914f1b1abad0a5980d55
Perlou <a87387875@163.com>
2016-06-02 12:57:10 +0800

    build

--- build/js/app.js
+++ build/js/app.js
@@ -3,28 +3,30 @@
  * @app.js
  */
 
+
 angular.module('todomvc', ['ngRoute', 'ngResource'])
-	.config(function ($routeProvider) {
-		'use strict';
 
-		var routeConfig = {
-			controller: 'TodoCtrl',
-			templateUrl: 'todomvc-index.html',
-			resolve: {
-				store: function (todoStorage) {
-					// Get the correct module (API or localStorage).
-					return todoStorage.then(function (module) {
-						module.get(); // Fetch the todo records in the background.
-						return module;
-					});
-				}
+.config(function($routeProvider){
+	'use strict';
+
+	var routeConfig = {
+		controller: 'TodoCtrl',
+		templateUrl: 'todomvc-index.html',
+		resolve: {
+			store: function(todoStorage){
+				return todoStorage.then(function(module){
+					module.get();
+					return module;
+				});
 			}
-		};
+		}
+	}
 
-		$routeProvider
-			.when('/', routeConfig)
-			.when('/:status', routeConfig)
-			.otherwise({
-				redirectTo: '/'
-			});
-	});
+	$routeProvider
+		.when('/', routeConfig)
+		.when('/:status', routeConfig)
+		.otherwise({
+			redirectTo: '/'
+		})
+
+});
--- build/js/controllers/todoCtrl.js
+++ build/js/controllers/todoCtrl.js
@@ -1,26 +1,34 @@
-/*global angular */
-
 /**
- * The main controller for the app. The controller:
- * - retrieves and persists the model via the todoStorage service
- * - exposes the model to the template and provides event handlers
+ * @author Perlou
+ * @todoCtrl.js
  */
+
 angular.module('todomvc')
 	.controller('TodoCtrl', function TodoCtrl($scope, $routeParams, $filter, store) {
 		'use strict';
+
+		// var todos = $scope.todos = store.todos;
+
+		// $scope.newTodo = '';
+		// $scope.editedTodo = null;
+
+		// $scope.$watch('todos', function () {
+		// 	$scope.remainingCount = $filter('filter')(todos, { completed: false }).length;
+		// 	$scope.completedCount = todos.length - $scope.remainingCount;
+		// 	$scope.allChecked = !$scope.remainingCount;
+		// }, true);
 
 		var todos = $scope.todos = store.todos;
 
 		$scope.newTodo = '';
 		$scope.editedTodo = null;
 
-		$scope.$watch('todos', function () {
-			$scope.remainingCount = $filter('filter')(todos, { completed: false }).length;
+		$scope.$watch('todos', function(){
+			$scope.remainingCount = $filter('filter')(todos, {completed: false}).length;
 			$scope.completedCount = todos.length - $scope.remainingCount;
 			$scope.allChecked = !$scope.remainingCount;
-		}, true);
+		});
 
-		// Monitor the current route for changes and adjust the filter accordingly.
 		$scope.$on('$routeChangeSuccess', function () {
 			var status = $scope.status = $routeParams.status || '';
 			$scope.statusFilter = (status === 'active') ?
@@ -50,13 +58,10 @@ angular.module('todomvc')
 
 		$scope.editTodo = function (todo) {
 			$scope.editedTodo = todo;
-			// Clone the original todo to restore it on demand.
 			$scope.originalTodo = angular.extend({}, todo);
 		};
 
 		$scope.saveEdits = function (todo, event) {
-			// Blur events are automatically triggered after the form submit event.
-			// This does some unfortunate logic handling to prevent saving twice.
 			if (event === 'blur' && $scope.saveEvent === 'submit') {
 				$scope.saveEvent = null;
 				return;
@@ -65,7 +70,6 @@ angular.module('todomvc')
 			$scope.saveEvent = event;
 
 			if ($scope.reverted) {
-				// Todo edits were reverted-- don't save.
 				$scope.reverted = null;
 				return;
 			}
